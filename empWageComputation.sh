cf2fdfb830836a533818c4b0bdd114cc0490d73f
lunainfotec <kirtimadan1997@gmail.com>
2023-03-18 00:34:46 +0530

    check employee present or not

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,20 @@
 
 
-
-
-
-
-
-
 echo " Welcome to Employee Wage Computation Program"
+
+
+
+ispresent=1;
+randomcheck="$((RANDOM%2))";
+
+if [ $ispresent -eq $randomcheck ];
+then
+echo "Employee is present";
+
+else
+
+echo "Empoloyee is not present";
+
+fi
 
 
