1376a6c8a003321989b4eac84aa7af91a0e49205
lunainfotec <kirtimadan1997@gmail.com>
2023-03-18 01:43:16 +0530

    add switch case program

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -36,3 +36,25 @@ else
 salary=0;
 
 fi
+
+isfulltime=2;
+isparttime=1;
+empcheck="$((RANDOM%3))";
+emprateperhr=20;
+
+case $empcheck in
+       $isfulltime)
+       emphr=8
+        ;; 
+
+       $isparttime)
+       emphr=4
+        ;;
+
+       *)
+
+salary=$(($emphr*$emprateperhr));
+
+esac
+
+
