172d947275aec3b7c2dda704ece5ae6ddfc3c2b9
lunainfotec <kirtimadan1997@gmail.com>
2023-03-18 01:11:26 +0530

    count employee salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,6 +8,8 @@ echo "Welcome to Employee Wage Computation Program";
 ispresent=1;
 randomcheck="$((RANDOM%2))";
 
+
+
 if [ $ispresent -eq $randomcheck ];
 then
 echo "Employee is present";
@@ -18,4 +20,19 @@ echo "Empoloyee is not present";
 
 fi
 
+ispresentt=1;
+randomcheckk="$((RANDOM%2))";
 
+if [ $ispresentt -eq $randomcheckk ];
+then
+       empratehr=20;
+        emphr=8;
+salary=$(($empratehr*$emphr));
+
+echo $salary;
+
+else
+
+salary=0;
+
+fi
