11b769b1b6e4ac4dda90197568a0dbbdebcf9540
lunainfotec <kirtimadan1997@gmail.com>
2023-03-19 22:58:00 +0530

    solsome issuse

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,96 @@
+
+
+
+echo "Welcome to Employee Wage Computation Program";
+
+
+
+ispresent=1;
+randomcheck="$((RANDOM%2))";
+
+
+
+if [ $ispresent -eq $randomcheck ];
+then
+echo "Employee is present";
+
+else
+
+echo "Empoloyee is not present";
+
+fi
+
+ispresentt=1;
+randomcheckk="$((RANDOM%2))";
+
+if [ $ispresentt -eq $randomcheckk ];
+then
+       empratehr=20;
+        emphr=8;
+salary=$(($empratehr*$emphr));
+
+echo $salary;
+
+else
+
+salary=0;
+
+fi
+
+isfulltime=2;
+isparttime=1;
+empcheck="$((RANDOM%3))";
+emprateperhr=20;
+
+case $empcheck in
+       $isfulltime)
+       emphr=8
+        ;; 
+
+       $isparttime)
+       emphr=4
+        ;;
+
+       *)
+
+salary=$(($emphr*$emprateperhr));
+
+esac
+
+
+isfulltime=2;
+isparttime=1;
+workingday=20;
+totalsalary=0;
+emprateperhrs=20;
+
+for(( day=0; day<=$workingday; day++ ))
+do
+
+empcheck=$((RANDOM%3));
+
+case $empcheck in
+    $isfulltime)
+    emphrs=8
+     ;;
+
+
+    $isparttime)
+    emphrs=4
+     ;;
+
+     *)
+esac
+
+
+salary=$(($emphrs*$emprateperhrs));
+totalsalary=$(($workingday*$salary));
+
+done
+
+
+
+
 isfulltime=2;
 isparttime=1;
 workingday=20;
